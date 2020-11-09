871f0ad0fe93e8f33beb9def49362d0517dbaa7e
LyuDongzhou <296938856@qq.com>
2020-11-09 08:28:07 +0800

    update interface

---
+++ interface.js
@@ -0,0 +1,85 @@
+const renderData = {
+    version: "1.0",
+    description: "",
+    container: {
+        width: 1920,
+        height: 1080
+    },
+    change: [{}],
+    pages: [{
+        components: [
+            {
+                id: 1,
+                name: "按钮1",
+                type: "Button",
+                layoutConfig: {
+                    zIndex: 1,
+                    top: 100,
+                    left: 100,
+                    rotation: Math.PI,
+                    width: 1,
+                    height: 1,
+                    opacity: 1
+                },
+                props: {
+                    bgUrl: "/image/btn.png"
+                },
+                animation: [
+
+                ],
+                events: [
+                    {
+                        type: "openDialog",
+                        value: "Dialog1"
+                    }
+                ],
+                children: []
+            }
+        ],
+    }]
+};
+const staticEditorData = {
+    Button: {
+        notSupport: [
+            {
+                type:"layout"
+            },
+            {
+                type:"animation"
+            }
+        ],
+        userDefine:[
+            {
+                title:"图片",
+                key:1,
+                props:{
+                    bgUrl:{
+                        editor:{
+                            type:"imageUploader",
+                            props:{}
+                        },
+                        defaultValue:{
+
+                        }
+                    }
+                }
+            },
+            {
+                title:"图片",
+                key:2,
+                props:{
+                    bgUrl:{
+                        label:"上传图片",
+                        editor:{
+                            type:"imageUploader",
+
+                        },
+                        defaultValue:{
+
+                        }
+                    }
+                }
+            }
+        ]
+    }
+};
