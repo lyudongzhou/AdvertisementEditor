945a62a2ba9b9691d0f656a9e655a2c0f4afba80
lvdongzhou <lvdongzhou@100tal.com>
2020-11-21 20:37:20 +0800

    rewrite build step

--- build/build.js
+++ build/build.js
@@ -1,6 +1,7 @@
 let projectName = process.argv[2]//是通过拿到环境参数来控制向project.js文件写入一个运行的名称
 let fs = require('fs')
 //写入
-fs.writeFileSync('./project.js', `exports.name = '${projectName}'`)
+const path = require('path');
+fs.writeFileSync(path.join(__dirname,'./project.js'), `exports.name = '${projectName}'`)
 let exec = require('child_process').execSync;
 exec('vue-cli-service build', {stdio: 'inherit'});
--- src/Render/App.vue
+++ src/Render/App.vue
@@ -1,44 +1,55 @@
 <template>
-  <div
-    id="app"
-    :style="{'position':'absolute','width':deviceWidth+'px','height':deviceHeight+'px','background-color':'lightgray'}"
-  >
-    <render
-      :renderData="renderData"
-      :currentPage="currentPage"
-      @click="onClick"
-      :designMode=true
-      :style="{
-          'top': '50%',
-          'left': '50%',
-          'transform': 'translate(-50%, -50%)'
-          }"
-    ></render>
-  </div>
+    <div
+        id="app"
+        :style="{
+            position: 'absolute',
+            width: deviceWidth + 'px',
+            height: deviceHeight + 'px',
+            'background-color': 'lightgray',
+        }"
+    >
+        <render
+            v-if="renderData"
+            :renderData="renderData"
+            :currentPage="currentPage"
+            @click="onClick"
+            :designMode="true"
+            :style="{
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+            }"
+        ></render>
+    </div>
 </template>
 <script>
 import render from "./render";
-import { renderData } from "../../interface";
+// import { renderData } from "../../interface";
 export default {
-  components: {
-    render
-  },
-  methods: {
-    onClick(dom){
-    }
-  },
-  created() {
-  },
-  data() {
-    return {
-      containerStyle: {},
-      deviceWidth: document.body.clientWidth,
-      deviceHeight: document.body.clientHeight,
-      renderData: renderData,
-      currentPage:0
-    };
-  }
+    components: {
+        render,
+    },
+    methods: {
+        onClick() {},
+    },
+    created() {
+        this.$axios({
+            url: "schema.json",
+            method: "get",
+            params: {},
+        }).then((res) => {
+            this.renderData = res.data;
+        });
+    },
+    data() {
+        return {
+            containerStyle: {},
+            deviceWidth: document.body.clientWidth,
+            deviceHeight: document.body.clientHeight,
+            renderData: null,
+            currentPage: 0,
+        };
+    },
 };
 </script>
-<style scoped>
-</style>
+<style scoped></style>
