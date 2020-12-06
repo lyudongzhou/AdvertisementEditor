52d23445e621de2d026c4dfd448e1f987f50b811
lvdongzhou <lvdongzhou@100tal.com>
2020-12-06 22:20:50 +0800

    update Popup & snapShot problems

--- src/Designer/components/pageCmp/ThumbnailShow1.vue
+++ src/Designer/components/pageCmp/ThumbnailShow1.vue
@@ -2,12 +2,12 @@
     <div class="thumbnailShow">
         <ul>
             <li
-                v-for="(page,index) in pages"
-                :class="page.id===currentPageId?'list_active':''"
+                v-for="(page, index) in pages"
+                :class="page.id === currentPageId ? 'list_active' : ''"
                 :key="page.id"
                 @click="switchCurrentPage(page)"
             >
-                <p class="list_index">{{index+1}}</p>
+                <p class="list_index">{{ index + 1 }}</p>
                 <p class="list_thumbnail">
                     <snapShotDisplay :id="page.id"></snapShotDisplay>
                 </p>
@@ -17,88 +17,86 @@
 </template>
 
 <script>
-import { mapMutations, mapGetters, mapState } from "../../store";
-import snapShotDisplay from "../snapShotDisplay";
-import manager from "../../manager/snapShot";
+import { mapMutations, mapGetters, mapState } from '../../store'
+import snapShotDisplay from '../snapShotDisplay'
+import manager from '../../manager/snapShot'
 export default {
-  name: "ThumbnailShow",
-  components: {
-    snapShotDisplay,
-  },
-  data() {
-    return {
-    };
-  },
-  computed: {
-    ...mapGetters(["pages"]),
-    ...mapState(["currentPageId"]),
-  },
-  created() {},
-  mounted() {
-  },
-  updated() {},
-  methods: {
-    ...mapMutations(["selectPage"]),
-    /**
-     * 点击缩略图切换页面
-     * @param {Object} page
-     */
-    switchCurrentPage(page) {
-      this.selectPage({id: page.id, currentPageType: 'page'});
+    name: 'ThumbnailShow',
+    components: {
+        snapShotDisplay,
     },
-  },
-  watch: {
-    pages(newValue) {
-      newValue.forEach((ele) => {
-        manager.addTask(ele.id);
-      });
+    data() {
+        return {}
     },
-  },
-};
+    computed: {
+        ...mapGetters(['pages']),
+        ...mapState(['currentPageId']),
+    },
+    created() {},
+    mounted() {},
+    updated() {},
+    methods: {
+        ...mapMutations(['selectPage']),
+        /**
+         * 点击缩略图切换页面
+         * @param {Object} page
+         */
+        switchCurrentPage(page) {
+            manager.addTask(this.currentPageId)
+            this.selectPage({ id: page.id, currentPageType: 'page' })
+        },
+    },
+    watch: {
+        // pages(newValue) {
+        //   newValue.forEach((ele) => {
+        //   });
+        // },
+    },
+}
 </script>
 
 <style lang="less" scoped>
 .thumbnailShow {
-  width: 100%;
-  height: 100%;
-  > ul {
     width: 100%;
     height: 100%;
-    text-align: center;
-    line-height: 157px;
-    li {
-      width: 100%;
-      height: 210px;
-      display: flex;
-      p {
-        margin-top: 21px;
-      }
-      .list_index {
-        width: 30px;
-        height: 30px;
-        background: #ffffff;
-        border-radius: 50%;
-        line-height: 30px;
+    > ul {
+        width: 100%;
+        height: 100%;
         text-align: center;
-        font-size: 16px;
-        font-family: PingFangSC-Regular, PingFang SC;
-        font-weight: 400;
-        color: #999999;
-        margin: 64px 38px 95px 31px;
-      }
-      .list_thumbnail {
-        width: 124px;
-        height: 157px;
-      }
+        line-height: 157px;
+        li {
+            width: 100%;
+            height: 210px;
+            display: flex;
+            p {
+                margin-top: 21px;
+            }
+            .list_index {
+                width: 30px;
+                height: 30px;
+                background: #ffffff;
+                border-radius: 50%;
+                line-height: 30px;
+                text-align: center;
+                font-size: 16px;
+                font-family: PingFangSC-Regular, PingFang SC;
+                font-weight: 400;
+                color: #999999;
+                margin: 64px 38px 95px 31px;
+            }
+            .list_thumbnail {
+                width: 124px;
+                height: 157px;
+            }
+        }
+        li:hover {
+            cursor: pointer;
+            background: #727272;
+        }
+        .list_active {
+            cursor: pointer;
+            background: #727272;
+        }
     }
-    li:hover {
-      cursor: pointer;
-      background: #727272;
-    }
-    .list_active {
-      cursor: pointer;
-      background: #727272;
-    }
-  }
 }
 </style>
--- src/Designer/components/popupCmp/PopupList.vue
+++ src/Designer/components/popupCmp/PopupList.vue
@@ -1,90 +1,93 @@
 <template lang="html">
-  <div class="popupList">
-      <ul>
-          <li
-              v-for="(dialog, index) in dialogs"
-              :key="dialog.id"
-          >
-              <p class="list_index">{{index+1}}</p>
-              <p class="list_window">
-                  <snapShotDisplay :id="dialog.id"></snapShotDisplay>
-              </p>
-          </li>
-      </ul>
-  </div>
+    <div class="popupList">
+        <ul>
+            <li
+                v-for="(dialog, index) in dialogs"
+                :key="dialog.id"
+                @click="switchCurrentPage(dialog)"
+            >
+                <p class="list_index">{{ index + 1 }}</p>
+                <p class="list_window">
+                    <snapShotDisplay :id="dialog.id"></snapShotDisplay>
+                </p>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import { mapMutations, mapGetters } from "../../store";
-import snapShotDisplay from "../snapShotDisplay";
-import manager from "../../manager/snapShot";
+import { mapMutations, mapGetters, mapState } from '../../store'
+import snapShotDisplay from '../snapShotDisplay'
+import manager from '../../manager/snapShot'
 
 export default {
-  name: "PopupList",
-  components: {
-    snapShotDisplay,
-  },
-  computed: {
-    ...mapGetters(["dialogs"]),
-  },
-  methods: {
-    ...mapMutations(["selectPage"]),
-    /**
-     * 点击缩略图切换页面
-     * @param {Object} page
-     */
-    switchCurrentPage(dialog) {
-      this.selectPage(dialog.id, 'dialog', 'dialog');
+    name: 'PopupList',
+    components: {
+        snapShotDisplay,
     },
-  },
-  watch: {
-    dialog (newValue) {
-      newValue.forEach((ele) => {
-        manager.addTask(ele.id);
-      });
+    computed: {
+        ...mapGetters(['dialogs']),
+        ...mapState(['currentPageId']),
     },
-  }
+    methods: {
+        ...mapMutations(['selectPage']),
+        /**
+         * 点击缩略图切换页面
+         * @param {Object} page
+         */
+        switchCurrentPage(dialog) {
+            manager.addTask(this.currentPageId)
+            this.selectPage({ id: dialog.id, currentPageType: 'dialog' })
+        },
+    },
+    watch: {
+        // dialog (newValue) {
+        //   newValue.forEach((ele) => {
+        //     manager.addTask(ele.id);
+        //   });
+        // },
+    },
 }
 </script>
 
 <style lang="less" scoped>
-  .popupList {
+.popupList {
     width: 100%;
     height: 100%;
     > ul {
-      width: 100%;
-      height: 100%;
-      text-align: center;
-      line-height: 157px;
-      li {
         width: 100%;
-        height: 210px;
-        display: flex;
-        p {
-          margin-top: 21px;
+        height: 100%;
+        text-align: center;
+        line-height: 157px;
+        li {
+            width: 100%;
+            height: 210px;
+            display: flex;
+            p {
+                margin-top: 21px;
+            }
+            .list_index {
+                width: 30px;
+                height: 30px;
+                background: #ffffff;
+                border-radius: 50%;
+                line-height: 30px;
+                text-align: center;
+                font-size: 16px;
+                font-family: PingFangSC-Regular, PingFang SC;
+                font-weight: 400;
+                color: #999999;
+                margin: 64px 38px 95px 31px;
+            }
+            .list_window {
+                width: 124px;
+                height: 157px;
+            }
         }
-        .list_index {
-          width: 30px;
-          height: 30px;
-          background: #ffffff;
-          border-radius: 50%;
-          line-height: 30px;
-          text-align: center;
-          font-size: 16px;
-          font-family: PingFangSC-Regular, PingFang SC;
-          font-weight: 400;
-          color: #999999;
-          margin: 64px 38px 95px 31px;
+        li:hover {
+            cursor: pointer;
+            background: #727272;
         }
-        .list_window {
-          width: 124px;
-          height: 157px;
-        }
-      }
-      li:hover {
-        cursor: pointer;
-        background: #727272;
-      }
     }
-  }
+}
 </style>
