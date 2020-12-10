09f1f8a4ee3fef3d61e876abde586a28bd5c5df3
lvdongzhou <lvdongzhou@100tal.com>
2020-12-10 21:10:43 +0800

    numberfield

--- src/Designer/components/designerCmp/editors/numberField/numberField.vue
+++ src/Designer/components/designerCmp/editors/numberField/numberField.vue
@@ -5,90 +5,107 @@
  * @Description: file content
 -->
 <template>
-  <el-input-number
-    v-model="text"
-    placeholder="请输入内容"
-    @change="onChange"
-    :min="config.props.min"
-    :max="config.props.max"
-  ></el-input-number>
+    <el-input-number
+        v-model="text"
+        placeholder="请输入内容"
+        @change="onChange"
+        @focus="onFocus"
+        @blur="onBlur"
+        :controls="false"
+        :min="config.props.min"
+        :max="config.props.max"
+    ></el-input-number>
 </template>
 
 <script>
 import { getPropByPath } from "@/utils";
 import { mapMutations, mapState } from "../../../../store";
-import Throttle from "../../../../manager/updateThrottle";
+// import Throttle from "../../../../manager/updateThrottle";
+import {
+    UPDATING_COMPONENT_PROPS,
+    BEFORE_UPDATE_COMPONENT_PROPS,
+    AFTER_UPDATE_COMPONENT_PROPS,
+} from "../../../../constant/schema";
 export default {
-  name: "numberField",
-  props: ["configData", "config"],
-  created() {},
-  data() {
-    return {
-      text: 0,
-      isNew: false,
-    };
-  },
-  computed: {
-    ...mapState(["currentComponentId"]),
-    dataText() {
-      return getPropByPath(this.configData, this.config.target);
+    name: "numberField",
+    props: ["configData", "config"],
+    created() {},
+    data() {
+        return {
+            text: 0,
+            isNew: false,
+        };
     },
-  },
-  methods: {
-    ...mapMutations(["updateSchema"]),
-    changeFun(type) {
-      this.updateSchema({
-        type: type,
-        value: {
-          [this.config.target]: this.text,
+    computed: {
+        ...mapState(["currentComponentId"]),
+        dataText() {
+            return getPropByPath(this.configData, this.config.target);
         },
-      });
     },
-    // onBlur() {
-    //   console.log("blur");
-    //   this.changeFun(AFTER_UPDATE_COMPONENT_PROPS);
-    // },
-    onChange() {
-      Throttle.update(this.config.target, this.text);
-      //   console.log("change");
-      //   UPDATE_COMPONENT_PROPS;
-      //   this.updateSchema({
-      //     type: window.isCommit ? "myCommit" : "myUpdate",
-      //     value: {
-      //       [this.config.target]: this.text,
-      //     },
-      //   });
+    methods: {
+        ...mapMutations(["updateSchema"]),
+        changeFun(type) {
+            this.updateSchema({
+                type: type,
+                value: {
+                    [this.config.target]: this.text,
+                },
+            });
+        },
+        // onBlur() {
+        //   console.log("blur");
+        //   this.changeFun(AFTER_UPDATE_COMPONENT_PROPS);
+        // },
+        onChange() {
+            console.log("change");
+            // Throttle.update(this.config.target, this.text);
+            //   console.log("change");
+            //   UPDATE_COMPONENT_PROPS;
+            //   this.updateSchema({
+            //     type: window.isCommit ? "myCommit" : "myUpdate",
+            //     value: {
+            //       [this.config.target]: this.text,
+            //     },
+            //   });
 
-      //   this.changeFun(UPDATE_COMPONENT_PROPS);
+            this.changeFun(UPDATING_COMPONENT_PROPS);
+        },
+        onFocus() {
+            console.log("focus");
+            this.changeFun(BEFORE_UPDATE_COMPONENT_PROPS);
+            this.beforeValue = this.dataText;
+        },
+        onBlur() {
+            console.log("blur");
+
+            if (this.beforeValue !== this.dataText) {
+                this.changeFun(AFTER_UPDATE_COMPONENT_PROPS);
+            }
+        },
+        // onInput() {
+        //   if (this.isMounted && !this.isNew) {
+        //     this.isNew = false;
+        //     console.log("input");
+        //     this.changeFun(AFTER_UPDATE_COMPONENT_PROPS);
+        //   }
+        // },
     },
-    // onFocus() {
-    //   console.log("focus");
-    //   this.changeFun(BEFORE_UPDATE_COMPONENT_PROPS);
-    // },
-    // onInput() {
-    //   if (this.isMounted && !this.isNew) {
-    //     this.isNew = false;
-    //     console.log("input");
-    //     this.changeFun(AFTER_UPDATE_COMPONENT_PROPS);
-    //   }
-    // },
-  },
-  watch: {
-    currentComponentId() {
-      console.log("currentComponentId");
-      this.isNew = true;
+    watch: {
+        currentComponentId() {
+            console.log("currentComponentId");
+            this.isNew = true;
+        },
+        dataText(text) {
+            this.text = parseFloat(text);
+        },
     },
-    dataText(text) {
-      this.text = parseFloat(text);
+    mounted() {
+        this.$nextTick(() => {
+            this.isMounted = true;
+        });
+        this.text = getPropByPath(this.configData, this.config.target);
+        console.log("mounted");
     },
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.isMounted = true;
-    });
-    this.text = getPropByPath(this.configData, this.config.target);
-    console.log("mounted");
-  },
 };
 </script>
 
--- src/Designer/manager/updateThrottle.js
+++ src/Designer/manager/updateThrottle.js
@@ -5,62 +5,63 @@
  * @Description: file content
  */
 import { mapMutations } from "../store";
-import store from "../store";
-import Vue from 'vue';
-import {AUTO_BEFORE_PROP,AUTO_UPDATING_PROP,AUTO_AFTER_PROP} from "../constant/schema";
+// import store from "../store";
+// import Vue from 'vue';
+// import {AUTO_BEFORE_PROP,AUTO_UPDATING_PROP,AUTO_AFTER_PROP} from "../constant/schema";
 console.log(mapMutations);
 // const updateSchema = mapMutations(["updateSchema"]).updateSchema;
 class Throttle {
-    constructor() {
-        this.unCommit = { path: null, value: null };//未提交的数据
-        Vue.observable(this.unCommit);
-    }
+    // constructor() {
+    //     this.unCommit = { path: null, value: null };//未提交的数据
+    //     Vue.observable(this.unCommit);
+    // }
     canUndo() {
-        return this.unCommit.path!==null;
+        return false;
     }
-    /**
-     * @description: 更新配置，如果路径不一致则提交
-     * @param {*} path
-     * @param {*} value
-     * @return {*}
-     */
-    update(path, value,flushNow = false) {
-        if (this.unCommit.path !== path) {
-            this.flush();
-            this.begin(path, value);
-        }
-        this.storage(path, value);
-        if(flushNow){
-            this.flush();
-        }
-    }
-    /**
-     * @description: 开始新的纪录，如果有上次有记录则提交
-     * @param {*} value
-     * @return {*}
-     */
-    flush() {
-        this.commit(AUTO_AFTER_PROP, this.unCommit.path,this.unCommit.value);
-        this.unCommit.path = null;
-        this.unCommit.value = null;
-    }
-    begin(path, value) {
-        this.commit(AUTO_BEFORE_PROP,path, value);
-        this.unCommit.path = path;
-        this.unCommit.value = value;
-    }
-    storage(path, value) {
-        this.commit(AUTO_UPDATING_PROP,path, value);
-        this.unCommit.value = value;
-    }
-    commit(type,path,value) {
-        if (path !== null) {
+    flush(){}
+    // /**
+    //  * @description: 更新配置，如果路径不一致则提交
+    //  * @param {*} path
+    //  * @param {*} value
+    //  * @return {*}
+    //  */
+    // update(path, value,flushNow = false) {
+    //     if (this.unCommit.path !== path) {
+    //         this.flush();
+    //         this.begin(path, value);
+    //     }
+    //     this.storage(path, value);
+    //     if(flushNow){
+    //         this.flush();
+    //     }
+    // }
+    // /**
+    //  * @description: 开始新的纪录，如果有上次有记录则提交
+    //  * @param {*} value
+    //  * @return {*}
+    //  */
+    // flush() {
+    //     this.commit(AUTO_AFTER_PROP, this.unCommit.path,this.unCommit.value);
+    //     this.unCommit.path = null;
+    //     this.unCommit.value = null;
+    // }
+    // begin(path, value) {
+    //     this.commit(AUTO_BEFORE_PROP,path, value);
+    //     this.unCommit.path = path;
+    //     this.unCommit.value = value;
+    // }
+    // storage(path, value) {
+    //     this.commit(AUTO_UPDATING_PROP,path, value);
+    //     this.unCommit.value = value;
+    // }
+    // commit(type,path,value) {
+    //     if (path !== null) {
             
-            mapMutations(["updateSchema"]).updateSchema.call({ $store: store }, {
-                type: type,
-                value: {[path]:value},
-            });
-        }
-    }
+    //         mapMutations(["updateSchema"]).updateSchema.call({ $store: store }, {
+    //             type: type,
+    //             value: {[path]:value},
+    //         });
+    //     }
+    // }
 }
 export default new Throttle();
