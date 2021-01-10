9ab7fb37c234e6544d2b9631f671963a4f08bce0
Appassionata <2214435468@qq.com>
2021-01-11 00:42:54 +0800

    反向代理

--- server/proxy.js
+++ server/proxy.js
@@ -3,8 +3,10 @@ const path = require("path");
 const fs = require("fs");
 
 const filter = function (pathname, req) {
-  const proxyPath = fs.readFileSync(path.resolve(__dirname, `./proxyConfig`), 'utf8');
-  const proxyPaths = proxyPath.split("\r\n").filter(x => x).map(x => `/api${x.trim()}`);
+  // const proxyPath = fs.readFileSync(path.resolve(__dirname, `./proxyConfig`), 'utf8');
+  // const proxyPaths = proxyPath.split("\r\n").filter(x => x).map(x => `/api${x.trim()}`);
+  delete require.cache[require.resolve('./proxyConfig')];
+  const proxyPaths = require('./proxyConfig');
   return proxyPaths.some(x => pathname === x);
 };
 module.exports = createProxyMiddleware(filter, {
