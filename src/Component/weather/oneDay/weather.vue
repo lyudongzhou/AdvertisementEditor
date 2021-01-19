0306674689eb316209199594832f28b634efff3f
jam- <18611748482@163.com>
2021-01-19 17:20:53 +0800

    fix

--- src/Component/weather/oneDay/weather.vue
+++ src/Component/weather/oneDay/weather.vue
@@ -49,7 +49,6 @@
           console.error('ip获取失败');
           return '';
         }
-        let ip = window.ip.ip;
         var num = 0;
         ip = ip.split(".");
         num = Number(ip[0]) * 256 * 256 * 256 + Number(ip[1]) * 256 * 256 + Number(ip[2]) * 256 + Number(ip[3]);
